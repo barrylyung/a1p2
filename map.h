c95a528fcce5aeb5d9c9d911fa7ad7a346e9b465
Barry Yung <byung@Barrys-MacBook-Pro.local>
2020-01-29 14:19:02 -0500

    added correct test file

--- map.h
+++ map.h
@@ -14,29 +14,18 @@ public:
   }
   //Adds an element to the map as a node pair
   void addElement(Node* elem) {
-    Node** nodeArr = new Node* [len_ +1];
-    if(elems_ == NULL) {
-      nodeArr[len_] = elem;
-    } else {
-      for(size_t i = 0; i < len_; i++) {
-        nodeArr[i] = elems_[i];
-      }
-      nodeArr[len_] = elem;
-    }
-    len_++;
-    elems_ = nodeArr;
   }
 
   //Removed the given key from the map
-  void removeElement(String* key);
-
+  void removeElement(Object* key);
 
   //Gets the value of the key
-  void getValue(String* key);
+  void getValue(Object* key);
 
   //Gets the length of the map
   void getLength();
 
   //Checks is the key is in the map
   bool isKeyIn(String* e);
+
 };
--- test-map.cpp
+++ test-map.cpp
@@ -6,6 +6,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void FAIL() { exit(1); }
+void OK(const char* m) { printf("Passed: %s\n", m); }
+void t_true(bool p) { if (!p) FAIL(); }
+void t_false(bool p) { if (p) FAIL(); }
+String* cast_string(Object *object) {
+	return dynamic_cast<String*>(object);
+}
+
+String* s1 = new String("A");
+String* s2 = new String("B");
+String* s3 = new String("C");
+
+Object* o1 = new Object();
+Object* o2 = new Object();
+Object* o3 = new Object();
+
+Node* n1 = new Node(s1, o1);
+Node* n2 = new Node(s2, o2);
+Node* n3 = new Node(s3, o3);
+
+Node* n4 = new Node(o1, s1);
+Node* n5 = new Node(o2, s2);
+Node* n6 = new Node(o3, s3);
+
+Map* m1 = new Map();
+
 //Test to check addElement
 void test1(){
   String* o1 = new String("Test");
@@ -23,15 +49,45 @@ void test2(){
   Node* n2 = new Node();
 }
 
-void test3(){
-  //Will test to check if a given value is in the map based on key
+//Tests Map Functions
+void testAddElem(){
+  Map* m1 = new Map();
+  m1->addElement(n1);
+  m1->addElement(n2);
+  m1->addElement(n3);
+  t_true(m1->len_ == 3);
+  t_true(m1->isKeyIn(s1) == 1);
+  t_true(m1->isKeyIn(s2) == 1);
+  t_true(m1->isKeyIn(s3) == 1);
+}
 
-  //Will test to check the length of the map
+void testRemoveElem() {
+  Map* m1 = new Map();
+  m1->addElement(n1);
+  m1->addElement(n2);
+  m1->addElement(n3);
+  t_true(m1->len_ == 3);
+  m1->removeElement(s1);
+  t_true(m1->len == 2);
+  t_true(m1->isKeyIn(s1) == 0);
+  t_true(m1->isKeyIn(s2) == 1);
+  t_true(m1->isKeyIn(s3) == 1);
+}
 
-  //Will check to test the equality of the map
+void testGetValue() {
+  Map* m1 = new Map();
+  m1->addElement(n1);
+  m1->addElement(n2);
+  m1->addElement(n3);
+  t_true(m1->getValue(s1) == o1);
+  t_true(m1->getValue(s2) == o2);
+  t_true(m1->getValue(s3) == o3);
 }
 
 int main(){
   test1();
   test2();
+  testAddElem();
+  testRemoveElem();
+  testGetValue();
 }
