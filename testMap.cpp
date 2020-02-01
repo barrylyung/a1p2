cd869b03addeea11664a88e87b8f453a7cb6fda9
Mark Thekkethala <33663144+mthek10@users.noreply.github.com>
2020-01-31 19:43:33 -0500

    Updates tests to include OK() at end to show pass

--- testMap.cpp
+++ testMap.cpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "object.h"
 #include "string.h"
 #include "node.h"
@@ -11,7 +13,7 @@ void OK(const char* m) { printf("Passed: %s\n", m); }
 void t_true(bool p) { if (!p) FAIL(); }
 void t_false(bool p) { if (p) FAIL(); }
 String* cast_string(Object *object) {
-	return dynamic_cast<String*>(object);
+    return dynamic_cast<String*>(object);
 }
 
 String* s1 = new String("A");
@@ -34,67 +36,72 @@ Map* m1 = new Map();
 
 //Test to check addElement
 void test1(){
-  String* o1 = new String("Test");
-  String* o2 = new String("Value");
-  Node* n1 = new Node(o1, o2);
-  Map* m1 = new Map();
-  m1 -> addElement(n1);
-  t_true(m1->len_ == 1);
-  t_false(m1->len_ == 3);
+    String* o1 = new String("Test");
+    String* o2 = new String("Value");
+    Node* n1 = new Node(o1, o2);
+    Map* m1 = new Map();
+    m1 -> addElement(n1);
+    t_true(m1->getLength() == 1);
+    t_false(m1->getLength() == 3);
+    OK("Test1");
 }
 
 //Tests to check node Constructor
 void test2(){
-  String* o1 = new String("Test");
-  String* o2 = new String("Value");
-  Node* n1 = new Node(o1, o2);
-  Node* n2 = new Node(s1, s2);
-  Map* m1 = new Map();
-  m1 -> addElement(n1);
-  m1 -> addElement(n2);
-  t_true(m1->len_ == 2);
-  t_false(m1->len_ == 0);
+    String* o1 = new String("Test");
+    String* o2 = new String("Value");
+    Node* n1 = new Node(o1, o2);
+    Node* n2 = new Node(s1, s2);
+    Map* m1 = new Map();
+    m1 -> addElement(n1);
+    m1 -> addElement(n2);
+    t_true(m1->getLength() == 2);
+    t_false(m1->getLength() == 0);
+    OK("Test2");
 }
 
 //Tests Map Functions
 void testAddElem(){
-  Map* m1 = new Map();
-  m1->addElement(n1);
-  m1->addElement(n2);
-  m1->addElement(n3);
-  t_true(m1->len_ == 3);
-  t_true(m1->isKeyIn(s1) == 1);
-  t_true(m1->isKeyIn(s2) == 1);
-  t_true(m1->isKeyIn(s3) == 1);
+    Map* m1 = new Map();
+    m1->addElement(n1);
+    m1->addElement(n2);
+    m1->addElement(n3);
+    t_true(m1->getLength() == 3);
+    t_true(m1->isKeyIn(s1) == 1);
+    t_true(m1->isKeyIn(s2) == 1);
+    t_true(m1->isKeyIn(s3) == 1);
+    OK("testAddElem");
 }
 
 void testRemoveElem() {
-  Map* m1 = new Map();
-  m1->addElement(n1);
-  m1->addElement(n2);
-  m1->addElement(n3);
-  t_true(m1->len_ == 3);
-  m1->removeElement(s1);
-  t_true(m1->len == 2);
-  t_true(m1->isKeyIn(s1) == 0);
-  t_true(m1->isKeyIn(s2) == 1);
-  t_true(m1->isKeyIn(s3) == 1);
+    Map* m1 = new Map();
+    m1->addElement(n1);
+    m1->addElement(n2);
+    m1->addElement(n3);
+    t_true(m1->getLength() == 3);
+    m1->removeElement(s1);
+    t_true(m1->getLength()== 2);
+    t_true(m1->isKeyIn(s1) == 0);
+    t_true(m1->isKeyIn(s2) == 1);
+    t_true(m1->isKeyIn(s3) == 1);
+    OK("testRemoveElem");
 }
 
 void testGetValue() {
-  Map* m1 = new Map();
-  m1->addElement(n1);
-  m1->addElement(n2);
-  m1->addElement(n3);
-  t_true(m1->getValue(s1) == o1);
-  t_true(m1->getValue(s2) == o2);
-  t_true(m1->getValue(s3) == o3);
+    Map* m1 = new Map();
+    m1->addElement(n1);
+    m1->addElement(n2);
+    m1->addElement(n3);
+    t_true(m1->getValue(s1) == o1);
+    t_true(m1->getValue(s2) == o2);
+    t_true(m1->getValue(s3) == o3);
+    OK("testGetValue");
 }
 
 int main(){
-  test1();
-  test2();
-  testAddElem();
-  testRemoveElem();
-  testGetValue();
+    test1();
+    test2();
+    testAddElem();
+    testRemoveElem();
+    testGetValue();
 }
